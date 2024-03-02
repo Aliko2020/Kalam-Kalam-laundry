e3583e626ade134507c9060928b0965275336e6f
rootgh <95168267+Aliko2020@users.noreply.github.com>
2024-03-02 02:29:32 -0800

    added slider image

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,26 @@
 import React from "react";
+import '../Pages/Home.css'
+import image1 from '../images/image1.jpg';
+import image2 from '../images/image2.jpg';
+import image3 from '../images/image3.png'
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section class="container">
+        <div class="slider-wrapper">
+            <div class="slider">
+                <img id="slide-1" src={image3} alt="image1" />
+                <img id="slide-2" src={image2} alt="image2" />
+                <img id="slide-3" src={image1} alt="image3" />
+            </div>
+            <div class="slider-nav">
+                <a href="slide-1"></a>
+                <a href="slide-2"></a>
+                <a href="slide-3"></a>
+            </div>
+        </div>
+    </section>
+    </>
   );
 };
